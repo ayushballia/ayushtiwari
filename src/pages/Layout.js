559820d02a64e8313dbc6e49df99b2fb6ed1fbfe
ayushballia <ayushtiwari@live.in>
2022-02-25 13:31:44 +0530

    update menu-btn

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,22 +1,42 @@
+import React from "react";
 import { Outlet, Link } from "react-router-dom";
+import { CgMenuRound, CgCloseO } from "react-icons/cg";
 
 const Layout = () => {
+  const [isOpen, setIsOpen] = React.useState(true);
+  const mobileNav = () => {
+    setIsOpen((isOpen) => !isOpen);
+    console.log(isOpen);
+  };
   return (
     <>
       <div className="navbar">
         <a href="/ayushtiwari">
           <h2 className="logo">Ayush</h2>
         </a>
+        <div className="menu-btn">
+          {isOpen ? (
+            <CgMenuRound onClick={mobileNav} />
+          ) : (
+            <CgCloseO onClick={mobileNav} />
+          )}
+        </div>
         <nav>
-          <ul>
+          <ul className={isOpen ? "menu-items" : ""}>
             <li>
-              <Link to="/">Home</Link>
+              <Link to="/" onClick={mobileNav}>
+                Home
+              </Link>
             </li>
             <li>
-              <Link to="/about">About</Link>
+              <Link to="/about" onClick={mobileNav}>
+                About
+              </Link>
             </li>
             <li>
-              <Link to="/contact">Contact</Link>
+              <Link to="/contact" onClick={mobileNav}>
+                Contact
+              </Link>
             </li>
           </ul>
         </nav>
